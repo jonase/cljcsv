9155cfd4d9f4299a06d29f533b76d088d23fc6fd
Jonas Enlund <jonas.enlund@gmail.com>
2010-10-21 20:51:38 +0300

    added project.clj and cleaned up some ^:private and ^:dynamic

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cljcsv "1.0.0-SNAPSHOT"
+  :description "Curly is a logic-free templating language for Clojure."
+  :url "http://github.com/jonase/cljcsv"
+  :dependencies [[org.clojure/clojure "1.3.0-master-SNAPSHOT"]]
+  :dev-dependencies [[mycroft/mycroft "0.0.2"]]
+  :source-path "src/main/clojure")
